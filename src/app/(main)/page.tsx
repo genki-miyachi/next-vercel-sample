b4bc5d89901df8c51da89ca420d2031e5a6d27eb
genki-miyachi <59684139+genki-miyachi@users.noreply.github.com>
2024-09-18 11:04:09 +0900

    add layout

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-	return <div>Hello Next.js</div>;
+import Link from "next/link";
+import { MdAddTask } from "react-icons/md";
+
+export default function HomePage() {
+	return (
+		<div className='text-gray-800 p-8 h-full overflow-y-auto pb-24'>
+			<header className='flex justify-between items-center'>
+				<h1 className='text-2cl font-bold flow item-center'>All Tasks</h1>
+				<Link
+					href='/new'
+					className='flex items-center gap-1 font-semibold border px-4 py-2 rounded-full shadow-sm text-white bg-gray-800 hober:bg-gray-700'>
+					<MdAddTask />
+					<div>Add Task</div>
+				</Link>
+			</header>
+			<div className='mt-8 flex flex-wrap gap-4'>Task Card</div>
+		</div>
+	);
 }
---
+++ src/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,14 @@
+const TaskCard = () => {
+  return <div>
+    <header>
+      <h1>タイトル</h1>
+      <div>タスクの説明</div>
+    </header>
+    <div>
+      <div>2024-12-31</div>
+      <div></div>
+    </div>
+  </div>
+}
+
+export default TaskCard;
